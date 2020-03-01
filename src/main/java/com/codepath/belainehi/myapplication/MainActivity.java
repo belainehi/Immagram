3a80304bd8ecf0489f0e6150cb89dcf664204aaf
belainehi <belainehi@berea.edu>
2020-03-01 18:08:21 -0500

    SignUp

--- src/main/java/com/codepath/belainehi/myapplication/MainActivity.java
+++ src/main/java/com/codepath/belainehi/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.codepath.belainehi.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,12 +14,14 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private File photoFile;
     private String photoFileName = "photo.jpg";
     private Button btnlogout;
+    private BottomNavigationView bottomNavigationView;
 
 
 
@@ -52,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         ivPostImage = findViewById(R.id.ivPostImage);
         btnSubmit = findViewById(R.id.btnSubmit);
         btnlogout = findViewById(R.id.btnSignOut);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
 
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +68,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
        // queryPosts();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                       // fragment = fragment1;
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_compose:
+                      //  fragment = fragment2;
+                        Toast.makeText(MainActivity.this, "Compose", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                       // fragment = fragment3;
+                        Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return false;
+            }
+        });
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ src/main/java/com/codepath/belainehi/myapplication/fragments/ComposeFragment.java
@@ -0,0 +1,29 @@
+package com.codepath.belainehi.myapplication.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.belainehi.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ComposeFragment extends Fragment {
+
+    public ComposeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+}
